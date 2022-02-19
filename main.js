b5062fe79f171edbd576580b8265fc3c0a86af37
eo-lind <eo.lind@gmail.com>
2022-02-18 19:51:02 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// individual tasks for getting ready
+
+const brushTeeth = () => {
+    console.log("Brush your teeth.");
+}
+
+const getDressed = () => {
+    console.log("Get dressed.");
+}
+
+const makeupOn = () => {
+    console.log("Put on makeup.");
+}
+
+const brushHair = () => {
+    console.log("Brush your hair.");
+}
+
+const makeCoffee = () => {
+    console.log("Make a lot of coffee.");
+}
+
+const drinkCoffee = () => {
+    console.log("Drink a lot of coffee!");
+}
+
+const teeth = brushTeeth()
+const dress = getDressed()
+const makeup = makeupOn()
+const brush = brushHair()
+const brewCoffee = makeCoffee()
+const coffee = drinkCoffee()
+
+
+
+//
